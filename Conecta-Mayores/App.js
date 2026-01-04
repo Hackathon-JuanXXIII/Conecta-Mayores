dac865a53036ff9df79094d059ba93de001d4d5e
Rocio1604 <rocpago@gmail.com>
2026-01-04 13:35:49 +0100

    EliminarUsuario

--- Conecta-Mayores/App.js
+++ Conecta-Mayores/App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Image,TouchableHighlight } from 'react-native';
+const icon=require('./assets/icon.png')
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Image source={icon} style={{width:100, height:100}} />
+            <Text>Open up App.js to start working on your app!</Text>
+            <TouchableHighlight onPress={() => alert("hola")} style={{backgroundColor:'green', borderRadius:3 }} >
+                 <Text>Boton</Text>
+            </TouchableHighlight>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
